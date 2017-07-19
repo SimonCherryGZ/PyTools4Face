e03e2688642a5a66223317709a1b3d7c77cb570d
SimonCherry <simoncherry@sina.com>
2017-07-19 09:56:32 +0800

    modify: 更新查找obj文件'v'和'f'对应关系的工具。

--- indicesFinder.py
+++ indicesFinder.py
@@ -1,3 +1,19 @@
+def get_obj_indices_size(path):
+    size = 0
+    with open(path, 'r') as f:
+        data = f.readlines()
+
+        for line in data:
+            odom = line.split()
+            prefix = odom[0]
+            if prefix.startswith('v', 0, len(prefix)) \
+                    and not prefix.startswith('vt', 0, len(prefix)) \
+                    and not prefix.startswith('vn', 0, len(prefix)):
+                size += 1
+
+    print size
+    return size
+
 
 def read_obj_indices(path):
     tmp = []
@@ -21,15 +37,25 @@ def read_obj_indices(path):
     return tmp
 
 
-def order_obj_indices(indices):
+def order_obj_indices(indices, size):
+    lists = [[] for t in range(size)]
+
+    i = 1
     for index in indices:
         print index
+        lists[int(index) - 1].append(i)
+        i += 1
+
+    # print lists
+    for element in lists:
+        print element
 
 
 if __name__ == '__main__':
-
     obj_dir = 'input/obj/'
     obj_name = 'base_face_uv.txt'
     obj_path = obj_dir + obj_name
     obj_indices = read_obj_indices(obj_path)
-    order_obj_indices(obj_indices)
+    indices_size = get_obj_indices_size(obj_path)
+
+    order_obj_indices(obj_indices, indices_size)
